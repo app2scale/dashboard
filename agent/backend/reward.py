7e9ee6e2e169ef5010a2d9ac0e84fcbaf0a63333
Huseyin Kaya <hkayabilisim@gmail.com>
2024-04-22 15:43:56 +0300

    features: inference mechanism

--- agent/backend/reward.py
+++ agent/backend/reward.py
@@ -1,28 +1,34 @@
-class Reward1():
-    '''Reward is simply CPU utilization, nothing more: higher cpu utilization, higher reward.
+class RewardHighCPUUsage():
+    '''Promotes high CPU usage.
+
+Reward is simply equivalent to cpu usage. When there is more than one pod running,
+the cpu usage is the average cpu usage over pods.
 
 #### Formula
 $$ r = c $$
-where $r$ and $c$ are reward and cpu utilizations, respectively.
+where $r$ and $c$ are reward and cpu usage, respectively.
     '''
 
     def __init__(self):
-        self.label = """Reward1: prefer high cpu utilization"""
+        self.label = """RewardHighCPUUsage"""
 
     def calculate(self, state):
         return state["cpu_usage"]
     
+class RewardLowCPUUsage():
+    '''Promotes low CPU usage.
 
-class Reward2():
-    '''Reward is the negative of CPU utilization: hence lower cpu utilization, higher reward.
+Reward is simply equivalent to the negative of cpu usage. When there is more than one pod running,
+the cpu usage is the average cpu usage over pods.
 
 #### Formula
 $$ r = - c $$
-where $r$ and $c$ are reward and cpu utilizations, respectively.
+where $r$ and $c$ are reward and cpu usage, respectively.
     '''
 
     def __init__(self):
-        self.label = """Reward2: prefer low cpu utilization"""
+        self.label = """RewardLowCPUUsage"""
 
     def calculate(self, state):
         return -state["cpu_usage"]
+    
